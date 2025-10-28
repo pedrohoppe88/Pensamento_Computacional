aa1ab164fbe06cefcb420958a620ab99390abff1
Pedro Henrique Hoppe <103641971+pedrohoppe88@users.noreply.github.com>
2025-10-28 17:24:46 -0300

    Add list manipulation instructions in 1.py

---
+++ Listas/1.py
@@ -0,0 +1,12 @@
+"""
+1. Dados as orientações até então, faça as seguintes instruções:
+Crie uma lista chamada animais que contenha os elementos 'cachorro', 'gato', 'papagaio', e 'tartaruga'
+Imprima o primeiro e o último elemento da lista animais
+Modifique o segundo elemento da lista animais para 'coelho'
+"""
+lista_animais = ['cachorro', 'gato', 'papagaio', 'tartaruga']
+
+print(lista_animais[0])
+print(lista_animais[-1]) 
+lista_animais[1] = 'coelho'  
+print(lista_animais) 
