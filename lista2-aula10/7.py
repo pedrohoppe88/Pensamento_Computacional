990ec73dc5d216b6a55f1210fc12de5bb609efaf
Pedro Henrique Hoppe <103641971+pedrohoppe88@users.noreply.github.com>
2025-11-06 14:00:00 -0300

    Add name checking functionality in 7.py

---
+++ lista2-aula10/7.py
@@ -0,0 +1,8 @@
+nomes = ["Ana", "Lucas", "Pedro", "Marcos", "João"]
+
+nome = input("Digite um nome: ")
+
+if nome in nomes:
+    print("O nome está na lista!")
+else:
+    print("O nome NÃO está na lista!")
